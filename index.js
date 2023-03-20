a811f7f580260e809478e47a6d8d45bf0e712230
edd-ie <ogendoeddie@gmail.com>
2023-03-20 18:35:36 +0300

    Long way find an alternative

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(arr, str){
+    return arr.filter(i => i.toLowerCase() == str.toLowerCase());
+}
+
+function fuzzyMatch(array, str){
+    let newArray = array.filter(i =>{ 
+        let letters = ""
+        for (let letter = 0; letter < str.length; letter++){
+            letters += i[letter];
+        }
+        return letters == str
+        })
+    return newArray
+}
+
+function matchName(arr, str){
+    let newArray = arr.filter(i => i.name == str)
+    return newArray;
+}
